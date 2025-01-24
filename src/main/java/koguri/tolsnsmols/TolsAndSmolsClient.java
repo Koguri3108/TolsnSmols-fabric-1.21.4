93446bdd2d565f4da2528f96f7e42546b3690e9f
Koguri3108 <Koguri.3108@gmx.at>
2025-01-24 22:47:34 +0100

    Initial commit

---
+++ src/main/java/koguri/tolsnsmols/TolsAndSmolsClient.java
@@ -0,0 +1,10 @@
+package koguri.tolsnsmols;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TolsAndSmolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/koguri/tolsnsmols/TolsAndSmolsDataGenerator.java
@@ -0,0 +1,11 @@
+package koguri.tolsnsmols;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TolsAndSmolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
